4603e26cb1f55546ff177fddce3ff0c032f8cc47
Michael Buschbeck <michael@buschbeck.net>
2021-01-28 19:57:52 +0100

    Fix version context variable (relative to docs)
    Version: 1.0.2

--- MychsMacroMagic.js
+++ MychsMacroMagic.js
@@ -1,7 +1,7 @@
 // Mych's Macro Magic by Michael Buschbeck <michael@buschbeck.net> (2021)
 // https://github.com/michael-buschbeck/mychs-macro-magic/blob/main/LICENSE
 
-const MMM_VERSION = "1.0.1";
+const MMM_VERSION = "1.0.2";
 
 on("chat:message", function(msg)
 {
@@ -93,7 +93,7 @@ class MychScriptContext
 {
     static players = {};
 
-    VERSION = MMM_VERSION;
+    version = MMM_VERSION;
 
     floor(value)
     {
